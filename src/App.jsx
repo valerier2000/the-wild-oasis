55056d47267845ee1bd2f88f94c8966d85c66cc8
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-16 09:38:43 +0200

    Add global styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
 
 const H1 = styled.h1`
   font-size: 2em;
   color: red;
 `;
 
+const StyledApp = styled.div`
+  background-color: green;
+  color: var(--color-grey-800);
+`;
+
 function App() {
   return (
-    <div>
-      <H1>Welcome to The Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Welcome to The Wild Oasis</H1>
+        <Button>Check In</Button>
+      </StyledApp>
+    </>
   );
 }
 
