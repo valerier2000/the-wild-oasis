0984623c34ddd03518a46cfd3b1806b6d6f38874
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-16 08:59:53 +0200

    Set up styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 2em;
+  color: red;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <H1>Welcome to The Wild Oasis</H1>
+    </div>
+  );
 }
 
 export default App;
