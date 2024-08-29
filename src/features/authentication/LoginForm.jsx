77cc18c7e76babe41f93a3f33b75ed17d20af7d6
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-29 15:24:42 +0200

    Remove hardcoded email and password values from LoginForm state

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("val@test.com");
-  const [password, setPassword] = useState("qwer1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
